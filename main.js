1200780375cea825b7ab56f57c37a66ec005b6a1
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 18:30:42 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+{
+  "first_name": "Taylor",
+  "last_name": "Hawkes",
+  "age": 31,
+  "address": {
+    "street": "954 Kazaam Lane",
+    "city": "Boulder",
+    "state": "CO",
+    "postalCode": "80303"
+  },
+  "emails": [
+    {
+      "type": "main",
+      "number": "th71852@gmail.com"
+    },
+    {
+      "type": "secondary",
+      "number": "taylordhawkes@gmail.com"
+    }
+  ]
+}
